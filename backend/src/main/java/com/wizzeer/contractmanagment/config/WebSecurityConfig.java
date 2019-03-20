eb1bc0ee235361e954dccc0977dc46e99e9aa6f9
Dominik Sowa <wizzeer@gmail.com>
2019-03-20 23:35:03 +0100

    csrf fix

--- backend/src/main/java/com/wizzeer/contractmanagment/config/WebSecurityConfig.java
+++ backend/src/main/java/com/wizzeer/contractmanagment/config/WebSecurityConfig.java
@@ -1,21 +1,34 @@
 package com.wizzeer.contractmanagment.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 @EnableWebSecurity
-public class WebSecurityConfig implements WebMvcConfigurer {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer  {
+	
 	@SuppressWarnings("deprecation")
 	@Bean
-	public UserDetailsService userDetailsService() throws Exception {
+	public UserDetailsService userDetailsService() {
 		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 		manager.createUser(
 				User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
 		return manager;
 	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    http
+	    	.csrf().disable();
+	    	
+	}
+	
 
 }
